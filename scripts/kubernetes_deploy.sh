bed01e5d328414ef2e61631a87e719c73ead11b0
kabalasu <kabalasu@adobe.com>
2020-08-10 19:17:08 -0700

    fix path

--- scripts/kubernetes_deploy.sh
+++ scripts/kubernetes_deploy.sh
@@ -6,28 +6,28 @@ minikube start
 sleep 10
 
 echo "create a persistant volume in cluster\n"
-kubectl apply -f ../kubernetes/my_postgres_persistant_volume.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_postgres_persistant_volume.yaml
 
 sleep 10
 
 echo "acquire the created persistant volume by using a persistant volume claim\n"
-kubectl apply -f ../kubernetes/my_postgres_persistant_volume_claim.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_postgres_persistant_volume_claim.yaml
 
 sleep 10
 
 echo "create postgres credentials secret vault\n"
-kubectl apply -f ../kubernetes/my_postgres_secret.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_postgres_secret.yaml
 
 sleep 10
 
 echo "create postgres deployment and service"
-kubectl apply -f ../kubernetes/my_postgres_deployment.yaml
-kubectl apply -f ../kubernetes/my_postgres_service.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_postgres_deployment.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_postgres_service.yaml
 
 sleep 10
 
-echo "Creating the translate deployment and service..."
-kubectl apply -f ../kubernetes/my_translate_app_deployment.yaml
+echo "Creating the translate deployment and service~/cloud-devops-project."
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_translate_app_deployment.yaml
 sleep 50
-kubectl apply -f ../kubernetes/my_translate_app_service.yaml
+kubectl apply -f ~/cloud-devops-project/kubernetes/my_translate_app_service.yaml
 
